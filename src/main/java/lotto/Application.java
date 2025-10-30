156c62f310032aa063194f181d714be9a84171e3
Kimgyuilli <rlarbdlf222@gmail.com>
2025-10-30 15:25:11 +0900

    feat(Application): 전체 흐름 연결

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        LottoMachine lottoMachine = new LottoMachine();
+
+        int purchaseAmount = inputView.readPurchaseAmount();
+        List<Lotto> lottos = lottoMachine.purchase(purchaseAmount);
+
+        outputView.printPurchaseCount(lottos.size());
+        outputView.printLottos(lottos);
+
+        WinningNumbers winningNumbers = new WinningNumbers(inputView.readWinningNumbers());
+        BonusNumber bonusNumber = new BonusNumber(inputView.readBonusNumber(), winningNumbers.getNumbers());
+
+        LottoResult result = new LottoResult(lottos, winningNumbers, bonusNumber);
+        outputView.printStatistics(result, purchaseAmount);
     }
 }
