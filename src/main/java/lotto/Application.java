8e73404253b8e897b3b710e143737166907303e3
Kimgyuilli <rlarbdlf222@gmail.com>
2025-10-30 20:08:58 +0900

    feat(Application): 각각 입력마다 루프 처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,11 +7,7 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            run();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        run();
     }
 
     private static void run() {
@@ -19,15 +15,55 @@ public class Application {
         OutputView outputView = new OutputView();
         LottoMachine lottoMachine = new LottoMachine();
 
-        int purchaseAmount = inputView.readPurchaseAmount();
-        List<Lotto> lottos = lottoMachine.purchase(purchaseAmount);
+        List<Lotto> lottos = readPurchaseAmountAndBuy(inputView, lottoMachine);
+        int purchaseAmount = calculatePurchaseAmount(lottos);
 
         outputView.printPurchaseCount(lottos.size());
         outputView.printLottos(lottos);
 
-        WinningNumbers winningNumbers = new WinningNumbers(inputView.readWinningNumbers());
-        BonusNumber bonusNumber = new BonusNumber(inputView.readBonusNumber(), winningNumbers.getNumbers());
+        WinningNumbers winningNumbers = readWinningNumbersWithRetry(inputView);
+        BonusNumber bonusNumber = readBonusNumberWithRetry(inputView, winningNumbers);
 
+        calculateAndPrintResult(lottos, winningNumbers, bonusNumber, purchaseAmount, outputView);
+    }
+
+    private static List<Lotto> readPurchaseAmountAndBuy(InputView inputView, LottoMachine lottoMachine) {
+        while (true) {
+            try {
+                int amount = inputView.readPurchaseAmount();
+                return lottoMachine.purchase(amount);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static int calculatePurchaseAmount(List<Lotto> lottos) {
+        return lottos.size() * 1000;
+    }
+
+    private static WinningNumbers readWinningNumbersWithRetry(InputView inputView) {
+        while (true) {
+            try {
+                return new WinningNumbers(inputView.readWinningNumbers());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static BonusNumber readBonusNumberWithRetry(InputView inputView, WinningNumbers winningNumbers) {
+        while (true) {
+            try {
+                return new BonusNumber(inputView.readBonusNumber(), winningNumbers.getNumbers());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static void calculateAndPrintResult(List<Lotto> lottos, WinningNumbers winningNumbers,
+                                                 BonusNumber bonusNumber, int purchaseAmount, OutputView outputView) {
         LottoResult result = new LottoResult(lottos, winningNumbers, bonusNumber);
         outputView.printStatistics(result, purchaseAmount);
     }
